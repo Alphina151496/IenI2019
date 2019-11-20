dc996089346d8b70e4774f88a04e3d23e775fdcf
Alphina151496 <151496@csgaugustinus.nl>
2019-11-20 08:40:16 +0000

    jyth

--- JS/H1/H1O01.js
+++ JS/H1/H1O01.js
@@ -1,21 +1,23 @@
 function setup() {
   canvas = createCanvas(1000,450);
-  background('orange');
+  background('grey');
   canvas.parent('processing');
   noLoop();
 }
 
 function draw() {
   // groene cirkel zonder rand
-  
   noStroke();
-  fill('grey');
+  fill('green');
   ellipse(225,225,400);
   
+    noStroke();
+  fill("white");
+  ellipse(225,225,300)
   // witte rechthoek met rode rand
   
-  stroke('red');
+  stroke('green');
   fill('white');
   strokeWeight(10);
-  rect(650,100,250,300);  
+  rect(125,125,200,200);  
 }
